7cef740198ba461214d8f04fdecf84bb93e8c272
s-tachibana1456 <CA01241005@st.kawahara.ac.jp>
2026-07-14 14:56:45 +0900

    サインの削除判定処理のファイルと一部実装

---
+++ LSR_Engine/src/Rules/LineClearRule.cs
@@ -0,0 +1,28 @@
+﻿using LSR_Engine.src.Event;
+using LSR_Engine.src.Logics;
+using LSR_Engine.src.MatchContext;
+using LSR_Engine.src.States;
+
+namespace LSR_Engine.src.Rules
+{
+    internal class LineClearRule
+    {
+        private readonly BoardState _boardState;
+        private readonly MatchConfig _matchConfig;
+        private readonly EventBus _bus;
+
+        public LineClearRule(BoardState boardState, MatchConfig matchConfig, EventBus bus)
+        {
+            _boardState = boardState;
+            _matchConfig = matchConfig;
+            _bus = bus;
+        }
+
+        public void Execute()
+        {
+            var deletableLines = BoardLogic.FindFilledLines(_boardState.GetBoard(), _matchConfig.MapSize);
+
+            
+        }
+    }
+}
