cff439fdd6c94a17d8987c438954a089950d132d
s-tachibana1456 <CA01241005@st.kawahara.ac.jp>
2026-07-16 14:54:55 +0900

    プレイヤーのピンチ状態を判断するRuleクラスの実装
    この2つのクラスは、1つのクラスに統合できる可能性がある。

---
+++ LSR_Engine/src/Rules/FieldSaturationRule.cs
@@ -0,0 +1,67 @@
+﻿using LSR_Engine.src.Cache;
+using LSR_Engine.src.Common;
+using LSR_Engine.src.Event;
+using LSR_Engine.src.Logger;
+using LSR_Engine.src.Logics;
+using LSR_Engine.src.MatchContext;
+using LSR_Engine.src.Rules.Interface;
+using LSR_Engine.src.States;
+
+namespace LSR_Engine.src.Rules
+{
+    internal readonly struct PlayerDangerEvent
+    {
+        public readonly bool IsDanger { get; }
+
+        public PlayerDangerEvent(bool isDanger)
+        {
+            IsDanger = isDanger;
+        }
+    }
+
+    internal class FieldSaturationRule : IRule<Empty, Empty>
+    {
+        private readonly NextBlockState nextBlock;
+        private readonly BoardState boardState;
+        private readonly BlockCache blockCache;
+        private readonly MatchConfig matchConfig;
+        private readonly EventBus eventBus;
+        private readonly ILogger logger;
+
+        public FieldSaturationRule(
+            NextBlockState nextBlock,
+            BoardState boardState,
+            BlockCache blockCache,
+            MatchConfig matchConfig,
+            EventBus eventBus,
+            ILogger logger
+        )
+        {
+            this.nextBlock = nextBlock;
+            this.boardState = boardState;
+            this.blockCache = blockCache;
+            this.matchConfig = matchConfig;
+            this.eventBus = eventBus;
+            this.logger = logger;
+        }
+
+        public Empty Execute(Empty empty)
+        {
+            Block next = nextBlock.NextBlock;
+            logger.Trace($"Player danger check start. Shape: {next.Shape}");
+
+            // 置けるときが true 置けない時が false
+            bool result = BoardLogic.CanPutNext(
+                boardState.GetBoard(),
+                blockCache.GetBlockCache(next.Shape),
+                matchConfig.MapSize
+            );
+
+            // 置けない時が危険状態なのでresultの否定を渡す。
+            eventBus.Publish(new PlayerDangerEvent(!result));
+            logger.Debug($"The Player is in danger: {!result}");
+
+            return default;
+        }
+    }
+}
---
+++ LSR_Engine/src/Rules/GameOverRule.cs
@@ -0,0 +1,49 @@
+﻿using LSR_Engine.src.BlockSystem;
+using LSR_Engine.src.Cache;
+using LSR_Engine.src.Common;
+using LSR_Engine.src.Event;
+using LSR_Engine.src.Logics;
+using LSR_Engine.src.MatchContext;
+using LSR_Engine.src.Rules.Interface;
+using LSR_Engine.src.States;
+
+namespace LSR_Engine.src.Rules
+{
+    internal readonly struct GameOverEvent { }
+
+    internal class GameOverRule : IRule<Empty, Empty>
+    {
+        private readonly BlockQueue _blockQueue;
+        private readonly BlockCache _blockCache;
+        private readonly BoardState _boardState;
+        private readonly MatchConfig _matchConfig;
+        private readonly EventBus _bus;
+
+        public GameOverRule(BlockQueue blockQueue, BlockCache blockCache, BoardState boardState, MatchConfig matchConfig, EventBus eventBus)
+        {
+            _blockQueue = blockQueue;
+            _blockCache = blockCache;
+            _boardState = boardState;
+            _matchConfig = matchConfig;
+            _bus = eventBus;
+        }
+
+        public Empty Execute(Empty empty)
+        {
+            Block nextBlock = _blockQueue.Peek();
+
+            bool result = BoardLogic.CanPutNext(
+                _boardState.GetBoard(),
+                _blockCache.GetBlockCache(nextBlock.Shape),
+                _matchConfig.MapSize
+            );
+
+            if (!result)
+            {
+                _bus.Publish(new GameOverEvent());
+            }
+
+            return default;
+        }
+    }
+}
