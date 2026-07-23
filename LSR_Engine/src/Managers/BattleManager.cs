218e1ee5cc7e49f8db6e94447782d4545b1243a8
SARADA7156 <tsskyepro@gmail.com>
2026-07-23 19:20:12 +0900

    クラスライブラリの移植完了
    SesstionManager類が完成し論理側のクラスライブラリのコードが全て移植完了した。

---
+++ LSR_Engine/src/Managers/BattleManager.cs
@@ -0,0 +1,25 @@
+﻿using LSR_Engine.src.MatchContext;
+
+namespace LSR_Engine.src.Managers
+{
+    internal class BattleManager : SesstionManager
+    {
+        protected readonly GameActionManager _sessionB;
+
+        public BattleManager(
+            GameActionManager sessionA,
+            GameActionManager sessionB,
+            MatchState matchState
+        )
+        : base(sessionA, matchState)
+        {
+            _sessionB = sessionB;
+        }
+
+        public new void Tick()
+        {
+            base.Tick();
+            _sessionB.DispatchActions();
+        }
+    }
+}
---
+++ LSR_Engine/src/Managers/SesstionManager.cs
@@ -0,0 +1,26 @@
+﻿using LSR_Engine.src.Common.Interface;
+using LSR_Engine.src.MatchContext;
+
+namespace LSR_Engine.src.Managers
+{
+    internal class SesstionManager : IUpdatable
+    {
+        protected readonly GameActionManager _sessionA;
+        protected readonly MatchState _matchState;
+
+        public SesstionManager(GameActionManager sessionA, MatchState matchState)
+        {
+            _sessionA = sessionA;
+            _matchState = matchState;
+        }
+
+        public void Tick()
+        {
+            // ゲームのティックを更新
+            _matchState.IncrementTick();
+
+            // Bufferからアクションを取得して処理させる
+            _sessionA.DispatchActions();
+        }
+    }
+}
